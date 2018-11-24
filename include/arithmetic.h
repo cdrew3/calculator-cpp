ee49407afe122cbc17eb2c6165b091d9480da2d1
Chris Drew <chris.drew.cd@gmai.com>
2018-11-23 21:26:27 -0700

    updated folder structure and added vscode tasks for building/running

---
+++ include/arithmetic.h
@@ -0,0 +1,37 @@
+namespace arithmetic {
+
+    bool equal(int, int);
+    bool equal(float, float);
+
+    // Addition
+    int add(int x, int y) {
+        return x + y;
+    }
+    float add(float x, float y) {
+        return x + y;
+    }
+
+    // Subtraction
+    int sub(int x, int y) {
+        return x - y;
+    }
+    float sub(float x, float y) {
+        return x - y;
+    }
+
+    // Multiply
+    int mul(int x, int y) {
+        return x * y;
+    }
+    float mul(float x, float y) {
+        return x * y;
+    }
+
+    // Divide
+    int div(int x, int y) {
+        return x / y;
+    }
+    float div(float x, float y) {
+        return x / y;
+    }
+}
